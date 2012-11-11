f523225d5c045188413e4c42e71cb2b59ff0c6ba
Borys <borwoj@gmail.com>
2012-11-11 13:22:51 +0100

    atrybuty

--- JPS_1_AST/src/datastore/SBAObject.java
+++ JPS_1_AST/src/datastore/SBAObject.java
@@ -3,19 +3,19 @@ package datastore;
 import edu.pjwstk.jps.datastore.ISBAObject;
 import edu.pjwstk.jps.datastore.OID;
 
+public class SBAObject implements ISBAObject {
 
-public class SBAObject implements ISBAObject{
+	public MyOID OID;
+	public String name;
 
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 	@Override
 	public OID getOID() {
-		// TODO Auto-generated method stub
-		return null;
+		return OID;
 	}
 
 }
